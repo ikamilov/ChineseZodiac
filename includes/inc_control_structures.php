be4fa36e34a1b1c99e595424c826d7975baf3cb8
ikamilov <kamilov6@gmail.com>
2017-05-08 10:49:16 -0500

    added

---
+++ includes/inc_control_structures.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Conditional Statements and Looping Structures</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.php">
+</head>
+<body>
+<div class="dynamic">
+	<h1>Conditional Statements and Looping Structures</h1>
+	<a href="#if_else">If...Else Statement</a>
+	<a href="#switch">Switch Statement</a>
+	<a href="#while_loop">While Loop</a>
+	<a href="#for_loop">For Loop</a>
+	<h2 id="if_else" >If...Else Statement</h2>
+	<p>This script allows the user to input a 4-digit birth year between 1912 and 2011. It determines what the Chinese Zodiac sign is for that year using if...else logic. A symbol is displayed for that sign and a file is updated with the number of people who have entered that year.</p>
+	<a href="BirthYear_ifelse.php">[Test the Script]</a>
+	<a  href="ShowSourceCode.php?source_file=BirthYear_ifelse.php">[View the Source Code]</a>
+	<h2 id="switch">Switch Statement</h2>
+	<p>This script allows the user to input a 4-digit birth year between 1912 and 2031. It determines what the Chinese Zodiac sign is for that year using switch logic. A symbol is displayed for that sign and a file is updated with the number of people who have entered that year. In addition, the script will display the name of a President who shared the same zodiac sign as well as the years for that sign between the date of 1900 and 2031.</p>
+	<a href="BirthYear_switch.php">[Test the Script]</a>
+	<a href="ShowSourceCode.php?source_file=BirthYear_switch.php">[View the Source Code]</a>
+	<h2  id="while_loop" >While Loop</h2>
+	<p>This script displays a table of the 12 Chinese Zodiac signs with an image for each sign and the birth years for each sign. It uses while logic to build the table.</p>
+	<a href="Chinese_Zodiac_while_loop.php">[Test the Script]</a>
+	<a href="ShowSourceCode.php?source_file=Chinese_Zodiac_while_loop.php">[View the Source Code]</a>
+	<h2 id="for_loop" >For Loop</h2>
+	<p>This script displays a table of the 12 Chinese Zodiac signs with an image for each sign and the birth years for each sign. It uses for loop logic to build the table.</p>
+	<a href="Chinese_Zodiac_for_loop.php">[Test the Script]</a>
+	<a href="ShowSourceCode.php?source_file=Chinese_Zodiac_for_loop.php">[View the Source Code]</a>
+
+</div>
+
+</body>
+</html>
