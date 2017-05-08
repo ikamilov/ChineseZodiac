55c8e5c61e7ea6b5d0bf3d5d8e4e7c15cbb512ab
ikamilov <kamilov6@gmail.com>
2017-05-08 10:48:58 -0500

    added

---
+++ includes/inc_button_nav.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="button-main">
+	<a href = "index.php?page=home_page"><img class="btn" src="images/HomePage.jpg" alt="[Home Page]" title="Home Page" style = "border:0" /></a><br />
+	<a href = "index.php?page=site_layout"><img class="btn" src="images/SiteLayout.jpg" alt="[Site Layout]" title="Site Layout" style = "border:0" /></a><br />
+	<a href = "index.php?page=control_structures"><img class="btn" src="images/ControlStructures.jpg" alt="[Control Structures]" title="Control Structures" style = "border:0" /></a><br />
+	<a href = "index.php?page=string_functions"><img class="btn" src="images/StringFunctions.jpg" alt="[String Functions]" title="String Functions" style = "border:0" /></a><br />
+	<a href = "index.php?page=web_forms"><img class="btn" src="images/WebForms.jpg" alt="[Web Forms]" title="Web Forms" style = "border:0" /></a><br />
+	<a href = "index.php?page=midterm_assesment"><img class="btn" src="images/MidtermAssesment.jpg" alt="[Midterm Assesment]" title="Midterm Assesment" style = "border:0" /></a><br />
+	<a href = "index.php?page=state_information"><img class="btn" src="images/StateInformation.jpg" alt="[State Information]" title="State Information" style = "border:0" /></a><br />
+	<a href = "index.php?page=user_templates"><img class="btn" src="images/UserTemplates.jpg" alt="[User Templates]" title="User Templates" style = "border:0" /></a><br />
+	<a href = "index.php?page=final_project"><img class="btn" src="images/FinalProject.jpg" alt="[Final Project]" title="Final Project" style = "border:0" /></a><br />
+</div>
+</body>
+</html>
