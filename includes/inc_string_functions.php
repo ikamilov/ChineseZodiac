1eea9bbf7fc1a8943217dfd181c5eba89b7a3c2a
ikamilov <kamilov6@gmail.com>
2017-05-10 23:48:27 -0500

    added

---
+++ includes/inc_string_functions.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>String and Character Functions</title>
+</head>
+<body>
+<div class="dynamic">
+<h1>String and Character Functions</h1>
+<a href="#string">String Functions</a>
+<a href="#char">Character Functions</a>
+
+<h2 id="string">String Functions</h2>
+<p>This script uses the levenshtein() function and the similar_text() function to calculate the difference between two strings. Because they use different algorithms, they can come up with different answers.</p>
+<a href="SimilarNames.php">[Test the Script]</a>
+<a href="ShowSourceCode.php?source_file=SimilarNames.php">[View the Source Code]</a>
+<h2 id="char" >Character Functions</h2>
+<p>This script determines which of the 12 Chinese zodiac sign names can be made using the letters in each of a set of phrases.</p>
+<a href="EmbeddedWorks.php">[Test the Script]</a>
+<a href="ShowSourceCode.php?source_file=EmbeddedWorks.php">[View the Source Code]</a>
+</div>
+
+</body>
+</html>
