bf37bcc3c7039d245a779418ac8c5c11d2f4198e
ikamilov <kamilov6@gmail.com>
2017-05-11 22:03:13 -0500

    added

---
+++ includes/inc_php_info.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="dynamic">
+<h2 style="text-align: center;">PHP in Web Development</h2>
+<p>PHP is commonly used by people who want to build a simple, dynamic website quickly and effortlessly. Since its debut in 1995, it’s become an extremely popular choice for web developers. So popular, in fact, that roughly 80% of all websites use it now, including many Fortune 500 companies and top technology startups. By the end of this track, you’ll have what it takes to begin your career in companies that use PHP, or even to build your own website!</p>
+<p>The power of PHP is exhibited in numerous websites operating with great response and appreciation on the World Wide Web. Given that PHP and HTML together work with great cohesion and give excellent results, it is only fair that PHP follows its partner in crime in the list. The fact that world’s most common eCommerce platform, Magneto has been written in PHP, speaks volumes about the utility of this language in web development.</p>
+<p>Server-side scripting. This is the most traditional and main target field for PHP. You need three things to make this work: the PHP parser (CGI or server module), a web server and a web browser. You need to run the web server, with a connected PHP installation. You can access the PHP program output with a web browser, viewing the PHP page through the server. All these can run on your home machine if you are just experimenting with PHP programming. See the installation instructions section for more information.</p>
+</div>
+</body>
+</html>
