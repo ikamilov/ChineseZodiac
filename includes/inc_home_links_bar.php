2b8be912d853c5cd02306d9ac892db5c6379777d
ikamilov <kamilov6@gmail.com>
2017-05-11 22:03:47 -0500

    added

---
+++ includes/inc_home_links_bar.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="dynamic" style="text-align: center;">
+	<a href="index.php?page=home_page&section=php">PHP</a>
+	<a href="index.php?page=home_page&section=zodiac">Chinese Zodiac</a>
+	</div>
+</body>
+</html>
