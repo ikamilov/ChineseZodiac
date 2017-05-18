06120feaf3fdcd6df2450afb037b2a549045a995
ikamilov <kamilov6@gmail.com>
2017-05-17 23:36:39 -0500

    added

---
+++ includes/inc_chinese_zodiac.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="dynamic">
+	<h2 style="text-align: center" > Chinese Zodiac</h2>
+
+       <p>The Chinese zodiac is the classification scheme that assigns an animal to each year in a repeating 12-year cycle. The 12-year cycle of Chinese zodiac is an approximation to the 11.86-year orbital period of jupiter, the largest planet of the solar system.[1] It is a scheme and systematic plan of future action that relates each year to an animal and its reputed attributes according to a 12-year cycle. It and its variations remain popular in several East Asian countries including China, Vietnam, Burma, Korea, Japan, Mongolia, Nepal, Bhutan, Sri Lanka, Cambodia, Laos and Thailand as well as the Buddhist calendar.</p>
+
+       <p>Identifying this scheme using the term "zodiac" reflects several superficial similarities to the Western zodiac: both have time cycles divided into 12 parts, each labels at least the majority of those parts with names of animals, and each is widely associated with a culture of ascribing a person's personality or events in his or her life to the supposed influence of the person's particular relationship to the cycle. Nevertheless, there are major differences: the Chinese 12-part cycle corresponds to years, rather than months. The Chinese zodiac is represented by 12 animals, whereas some of the signs in the Western zodiac are not animals, despite the implication of the Greek etymology of "zodiac". The animals of the Chinese zodiac are not associated with constellations spanned by the ecliptic plane.</p>
+
+       <p>Within the Four Pillars, the year is the pillar representing information about the person's family background and society or relationship with their grandparents. The person's age can also be easily deduced from the sign of the person, the current sign of the year and the person's perceived age (teens, mid 20's, 40's and so on). For example, a person who is a Tiger is either 12, 24, 36 or 48 years old in 2010, the year of the Tiger. In 2011, the year of the Rabbit, that person is one year older.The following table shows the 60-year cycle matched up to the Western calendar for the years 1924–2043 (see Sexagenary cycle article for years 1804–2043). The sexagenary cycle begins at lichun 'about February 4' according to some astrological sources.[4]</p>
+
+       <p>Within the Four Pillars, the year is the pillar representing information about the person's family background and society or relationship with their grandparents.</p>
+
+<p>Source: http://www.chinahighlights.com/travelguide/chinese-zodiac/</p>
+</div>
+</body>
+</html>
+	
